702d9fd308f8870a9111d78e0d4afb4c5527dd10
jason <jason_w96@163.com>
2025-02-08 15:52:45 +0800

    for 迭代

--- go/leetcode1/main.go
+++ go/leetcode1/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println(loopadd(3))
+}
+func loopadd(n int) int {
+	result := 0
+	for i := 0; i <= n; i++ {
+		result += i
+	}
+	return result
 }
