6ba963ef9a55900be42cef46bba485c7d7f9d4bd
jason <jason_w96@163.com>
2025-02-08 15:59:00 +0800

    while 迭代

--- rust/leetcode1/src/main.rs
+++ rust/leetcode1/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", loopfn(3))
+}
+
+fn loopfn(n: i32) -> i32 {
+    let mut res = 0;
+    for i in 1..=n {
+        res += i;
+    }
+    res
+}
+fn whilefn(n: i32) -> i32 {
+    let mut res = 0;
+    let mut i = 1;
+    while i <= n {
+        res += i;
+        i += 1;
+    }
+    res
 }
--- rust/leetcode1/target/debug/deps/leetcode1-8bf3d88b9a34aea6.d
+++
@@ -1,5 +0,0 @@
-/Volumes/Code/code/project_dir/leetcode-practise/rust/leetcode1/target/debug/deps/leetcode1-8bf3d88b9a34aea6: src/main.rs
-
-/Volumes/Code/code/project_dir/leetcode-practise/rust/leetcode1/target/debug/deps/leetcode1-8bf3d88b9a34aea6.d: src/main.rs
-
-src/main.rs:
